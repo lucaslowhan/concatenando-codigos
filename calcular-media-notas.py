99778c6c675b8b8f1a8f8cf9f37119674d72f664
lucaslowhan <lucas.lowhan@hotmail.com>
2024-12-20 06:38:09 -0300

    Adicionando o quinto desafio

---
+++ calcular-media-notas.py
@@ -0,0 +1,5 @@
+notaA = float(input("Digite a primeira nota: "))
+notaB = float(input("Digite a segunda nota: "))
+notaC = float(input("Digite a terceira nota: "))
+media = (notaA + notaB + notaC) / 3
+print("A média das notas é {:.2f}".format(media))
